fe76177d52fad93b163cc09c577e1ba850467869
Juan Guerrero <juanmatiasg@hotmail.com>
2019-04-15 12:30:46 -0300

    Cambie el if-else por un switch

--- src/ar/edu/unlampb2/Calculadora.java
+++ src/ar/edu/unlampb2/Calculadora.java
@@ -1,7 +1,5 @@
 package ar.edu.unlampb2;
 
-
-
 import java.util.Scanner;
 
 public class Calculadora {
@@ -22,24 +20,32 @@ public class Calculadora {
 			System.out.println("Ingrese el segundo numero");
 			b = teclado.nextInt();
 
-			System.out.println("Ingrese su operación \n 1) para sumar \n 2) para restar \n 3) para multiplicar \n 4) para dividir");
+			System.out.println(
+					"Ingrese su operación \n 1) para sumar \n 2) para restar \n 3) para multiplicar \n 4) para dividir");
 
 			opcion = teclado.nextInt();
 		}
 
 		while (opcion < 1 && opcion > 4);
 
-		if (opcion.equals(1)) {
-			resultado = a + b;
-		} else if (opcion.equals(2)) {
-			resultado = a - b;
-		} else if (opcion.equals(3)) {
-			resultado = a * b;
-		} else {
+		switch (opcion) {
+
+		case 1:
+			System.out.println("El resultado de la suma es: "+(resultado=a+b));
+			break;
+		case 2:
+			System.out.println("El resultado de la resta es: "+(resultado=a-b));
+			break;
+		case 3:
+			System.out.println("El resultado de la multiplicacion es: "+(resultado=a*b));
+			break;
+		case 4:
+			if (b != 0) {
+				System.out.println("El resultado de la division es: "+(resultado=a/b));
+			} else {
+				System.out.println("No se puede dividir por Cero ");
+			}
 			
-			resultado = a / b;
 		}
-		System.out.println("El resultado es " + resultado.toString());
 	}
 }
-
